6a5adb1b002b9fd9ca61e4f03cb27810e00ee2ad
Aditya Kushwaha <adityagr488@gmail.com>
2024-02-04 14:53:19 +0530

    Refactored Orders

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -3,10 +3,11 @@ import { StatusCodes } from "http-status-codes";
 
 import { AuthenticatedRequest } from "../middlewares/isAuthenticated";
 import Order, { OrderStatus } from "../models/order.model";
+import Cart from "../models/cart.model";
 
 export const getOrders = async(req: AuthenticatedRequest, res: Response)=>{
     const user = req.user;
-    const orders = await Order.find({user: user}).populate('address');
+    const orders = await Order.find({user: user}).populate('address').sort({ order_date: 'desc' });
     res.status(StatusCodes.OK).json(orders);
 }
 
@@ -15,7 +16,7 @@ export const placeOrder = async(req: AuthenticatedRequest, res: Response)=>{
     const orderDetails = req.body;
     const order = {
         user: user,
-        date: orderDetails.date,
+        order_date: orderDetails.order_date,
         items: orderDetails.items,
         totalItems: orderDetails.totalItems,
         totalAmount: orderDetails.totalAmount,
@@ -26,9 +27,9 @@ export const placeOrder = async(req: AuthenticatedRequest, res: Response)=>{
     try{
         // TODO: Check for payment status
         await Order.create(order);
+        await Cart.updateOne({user: user}, { $set: { items: [] } });
         res.status(StatusCodes.CREATED).end();
     }catch(e){
-        console.log(e);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error: "Error creating order"});
     }
 }
